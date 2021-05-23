ce9f94f5693a160b57688671ab6f1503584c1264
Breyanna Kerley <breyanna.kerley@gmail.com>
2021-05-23 19:09:19 -0400

    brey pushing update and delete endpoints

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,26 +1,16 @@
-let express = require('express');
-let router = express.Router();
-let validateSession = require('../middleware/validate-session');
+const express = require('express');
+const router = express.Router();
+const validateSession = require('../middleware/validateSession');
 const recipe = require('../models/recipe');
 
-router.post('/create', validateSession, (req, res) => {
-    const recipeEntry = {
-        name: req.body.recipe.title,
-        ingredients: req.body.recipe.ingredients,
-        preparation: req.body.recipe.preparation,
-        time: req.body.recipe.time,
-        owner: req.user.id
-    }
-Recipe.create(recipeEntry)
-    .then(recipe => res.status(200).json(recipe))
-    .catch(err => res.status(500).json({ error: err}))
-})
 
-router.get('/', function(req,res){
+router.get('/', function(req, res){
     Recipe.findAll()
     .then(recipes => res.status(200). json(recipes))
     .catch(err => res.status(500).json({error: err}))
 });
+
+// Eric: call your own list of recipes and saved to your userid
 
 router.get('/my-recipes', validateSession, (req, res) => {
     let userid = req.user.id
@@ -31,13 +21,41 @@ router.get('/my-recipes', validateSession, (req, res) => {
     .catch(err => res.status(500).json({error: err}))
 });
 
-router.get('/:title', function(req,res) {
-    let title = req.params.title;
+// enable you to search by title
+router.get('/:name', function(req,res) {
+    let name = req.params.name;
     Recipe.findAll({
-        where: {title: title}
+        where: {name: name}
     })
     .then(recipes => res.status(200).json(recipes))
     .catch(err => res.status(500),json({error: err}))
 })
+// Brey's PUT ***********
+
+router.put("/update/:entryId", validateSession, function (req, res) {
+    const updateRecipePost = {
+        name: req.body.recipe.name,
+        ingredients: req.body.recipe.ingredients,
+        preparation: req.body.recipe.preparation,
+        time: req.body.recipe.time,
+        owner: req.body.recipe.owner
+    };
+
+    const query = { where: { id: req.params.entryId, owner: req.user.id } };
+
+    Recipe.update(updateRecipePost, query)
+        .then((recipes) => res.status(200).json(recipes))
+        .catch((err) => res.status(500).json({ error: err }));
+});
+
+// Brey's DELETE ***********
+
+router.delete("/delete/:id", validateSession, function (req, res) {
+    const query = {where: { id: req.params.id, owner: req.user.id } };
+
+    recipe.destroy(query)
+        .then(() => res.status(200).json({ message: "Recipe Entry Removed" }))
+        .catch((err) => res.status(500).json({ error: err }));
+});
 
 module.exports = router;
