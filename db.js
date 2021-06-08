166af5a8fe498ecd68533476031f53f790f9834c
Dex778 <43555123+dex778@users.noreply.github.com>
2021-06-07 23:31:13 -0400

    env upadte

--- db.js
+++ db.js
@@ -1,10 +1,18 @@
 const Sequelize = require('sequelize');
 
 
-const db = new Sequelize(process.env.DB_NAME, 'postgres', process.env.DB_PASS, {
-    host: 'localhost',
-    dialect: 'postgres'
-});
+const db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    ssl: true,
+    protocol: "postgres",
+    logging: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 
 
 module.exports = db;
